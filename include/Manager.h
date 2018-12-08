a7c7f4d60db8e651821e6644f7ca7ecde4f2367f
kshitij12345 <kshitijkalambarkar@gmail.com>
2018-12-08 12:56:23 +0530

    update documentation

--- include/Manager.h
+++ include/Manager.h
@@ -22,33 +22,31 @@ struct Node{
 
 	Node(int id,std::vector<int>depends_on,std::vector<int>dependents,void (*func)());
 	void operator()();
-	//std::thread ReturnFunc();
 };
 
 struct Manager{
-	//Nodes whose dependencies are resolved and can be run
-	std::queue<int> to_run;
-
-	//Contains the id of completed nodes.
-	std::vector<int> completed_nodes;
-
-	//Mutex to make sure the update of to_run and completed are atomic
-	std::mutex update_lock;
-
 	// Vector of all the nodes to run.
 	std::vector<Node> nodes;
 
+	// Nodes whose dependencies are resolved and can be run
+	std::queue<int> to_run;
 	std::set<std::reference_wrapper<int>> to_run_set;
+
+	// Contains the id of completed nodes.
+	std::vector<int> completed_nodes;
 	std::set<int> completed_set;
 
-	//Method which updates the to_run and completed
+	// Mutex to make sure the update of to_run and completed are atomic
+	std::mutex update_lock;
+
+	// Method which atomically updates the to_run and completed
 	void update(std::vector<int> dependents,int id);
 
 	// returns bool representing if all parents
 	// of current indexed node have finished
 	bool if_all_parents_fin(int i);
 
-	void execute();
+	void execute(int src_node = 0);
 
 };
 
