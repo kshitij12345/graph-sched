52ff5bbaf7cd5ff44a6488e99eec3d76e677b88f
kshitij12345 <kshitijkalambarkar@gmail.com>
2018-12-09 00:27:51 +0530

    rename depends_on->parents, dependents->children

--- include/Manager.h
+++ include/Manager.h
@@ -16,8 +16,8 @@ used to solve the dependencies
 
 struct BaseNode{
 	int id;
-	std::vector<int> depends_on;
-	std::vector<int> dependents;
+	std::vector<int> parents;
+	std::vector<int> children;
 	
 	virtual ~BaseNode(){}
 	virtual void call() const = 0;
@@ -27,10 +27,10 @@ template <typename F>
 struct Node : BaseNode{
 	F func;
 
-	Node(int id,std::vector<int>depends_on,std::vector<int>dependents,F func){
+	Node(int id,std::vector<int>parents,std::vector<int>children,F func){
 		this->id = id;
-		this->depends_on = depends_on;
-		this->dependents = dependents;
+		this->parents = parents;
+		this->children = children;
 		this->func = func;
 	}
 
@@ -43,25 +43,25 @@ struct Node : BaseNode{
 
 template <typename N1, typename N2>
 N1& operator<<(N1& self, N2& node){
-	self.depends_on.push_back(node.id);
+	self.parents.push_back(node.id);
 	return self;
 }
 
 template <typename N1, typename N2>
 N1& operator>>(N1& self, N2& node){
-	self.dependents.push_back(node.id);
+	self.children.push_back(node.id);
 	return self;
 }
 
 template <typename F>
 Node<F> make_node(int id,F func){
-	std::vector<int>depends_on;
-	std::vector<int>dependents;
-	return Node<F>(id, depends_on, dependents, func);
+	std::vector<int>parents;
+	std::vector<int>children;
+	return Node<F>(id, parents, children, func);
 }
 
 struct Manager{
-	// Vector of all the nodes to run.
+	// map to hold of all the nodes.
 	std::map<int, std::shared_ptr<BaseNode>> nodes;
 
 	// Nodes whose dependencies are resolved and can be run
@@ -81,7 +81,7 @@ struct Manager{
 	}
 
 	// Method which atomically updates the to_run and completed
-	void update(std::vector<int> dependents,int id);
+	void update(std::vector<int> children,int id);
 
 	// returns bool representing if all parents
 	// of current indexed node have finished
--- src/Manager.cpp
+++ src/Manager.cpp
@@ -13,10 +13,10 @@
 // }
 
 bool Manager::if_all_parents_fin(int i){
-	std::vector<int> depends_on = (nodes[i])->depends_on;// Nodes are indexed rather than mapped. Change
+	std::vector<int> parents = (nodes[i])->parents;// Nodes are indexed rather than mapped. Change
 
-	for(int j=0; j < depends_on.size(); j++){
-		if ((completed_set.find(depends_on[j])) == completed_set.end()){
+	for(int j=0; j < parents.size(); j++){
+		if ((completed_set.find(parents[j])) == completed_set.end()){
 			// If not present
 			return false;
 		}
@@ -26,16 +26,16 @@ bool Manager::if_all_parents_fin(int i){
 }
 
 //Update To_Run and Completed atomically.
-void Manager::update(std::vector<int> dependents,int id){
+void Manager::update(std::vector<int> children,int id){
 	{
 		std::lock_guard<std::mutex> Lock(update_lock);
 
 		completed_nodes.push_back(id);
 		completed_set.insert(id);
 		
-		for(int i = 0;i<dependents.size();i++){
-			if(if_all_parents_fin(dependents[i])){
-				to_run.push(dependents[i]);
+		for(int i = 0;i<children.size();i++){
+			if(if_all_parents_fin(children[i])){
+				to_run.push(children[i]);
 				to_run_set.insert(std::ref(to_run.back()));
 			}
 					
@@ -59,7 +59,7 @@ void Manager::execute(int src_node_idx){
 				// start all runnable nodes.
 				int id = to_run.front();
 				to_run.pop();
-				auto update_func = [=]{nodes[id]->call(); this->update(nodes[id]->dependents, id);};
+				auto update_func = [=]{nodes[id]->call(); this->update(nodes[id]->children, id);};
 				Threads.push_back(std::thread(update_func));
 			}
 
