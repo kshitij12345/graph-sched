63f8fccf78611f0d2dda7148d5174db8d6da4ca7
kshitij12345 <kshitijkalambarkar@gmail.com>
2018-12-16 19:54:25 +0530

    extend dsl for tuple to tuple

--- test/test.cpu.cpp
+++ test/test.cpu.cpp
@@ -17,29 +17,40 @@ TEST_CASE( "Node DSL constructs graph correctly.", "[node-dsl]" ) {
 	auto& node1 = m.append_node(1, dummy_func);
 	auto& node2 = m.append_node(2, dummy_func);
 	auto& node3 = m.append_node(3, dummy_func);
+	auto& node4 = m.append_node(4, dummy_func);
+	auto& node5 = m.append_node(5, dummy_func);
 
 	// Use the Node DSL to construct a graph.
-	node0 >> (node1, node2) >> node3;
+	node0 >> (node1, node2) >> (node3, node4, node5);
 
 	// Expected parent/child relationship of the nodes.
 	std::set<int> expected_node0_parents {};
 	std::set<int> expected_node0_children {1, 2};
 	std::set<int> expected_node1_parents {0};
-	std::set<int> expected_node1_children {3};
+	std::set<int> expected_node1_children {3, 4, 5};
 	std::set<int> expected_node2_parents {0};
-	std::set<int> expected_node2_children {3};
-	std::set<int> expected_node3_parents {1,2};
+	std::set<int> expected_node2_children {3, 4, 5};
+	std::set<int> expected_node3_parents {1, 2};
 	std::set<int> expected_node3_children {};
-	
+	std::set<int> expected_node4_parents {1, 2};
+	std::set<int> expected_node4_children {};
+	std::set<int> expected_node5_parents {1, 2};
+	std::set<int> expected_node5_children {};
+
 	REQUIRE(node0.parents == expected_node0_parents);
 	REQUIRE(node1.parents == expected_node1_parents);
 	REQUIRE(node2.parents == expected_node2_parents);
 	REQUIRE(node3.parents == expected_node3_parents);
+	REQUIRE(node4.parents == expected_node4_parents);
+	REQUIRE(node5.parents == expected_node5_parents);
 
 	REQUIRE(node0.children == expected_node0_children);
 	REQUIRE(node1.children == expected_node1_children);
 	REQUIRE(node2.children == expected_node2_children);
 	REQUIRE(node3.children == expected_node3_children);
+	REQUIRE(node4.children == expected_node4_children);
+	REQUIRE(node5.children == expected_node5_children);
+	
 }
 
 TEST_CASE( "Graph execution order is correct.", "[manager]" ) {
