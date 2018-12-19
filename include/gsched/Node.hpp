aaafbf8f1d701ea38bffb132e622387cec212d7f
kshitij12345 <kshitijkalambarkar@gmail.com>
2018-12-19 20:31:39 +0530

    use vector to chain graph

--- include/gsched/Node.hpp
+++ include/gsched/Node.hpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <set>
 #include <functional>
+#include <algorithm>
 
 namespace gsched{
 
@@ -10,7 +11,9 @@ namespace gsched{
 // the below using statement.
 struct BaseNode;
 
-using vec_BaseNode_ref = std::vector<std::reference_wrapper<BaseNode>>;
+using BaseNode_ref = std::reference_wrapper<BaseNode>;
+
+using vec_BaseNode_ref = std::vector<BaseNode_ref>;
 
 struct BaseNode{
 	// This is the base struct for Nodes.
@@ -48,70 +51,76 @@ inline BaseNode& operator>>(BaseNode& lhs, BaseNode& rhs){
 	return rhs;
 }
 
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-for_each_node(const std::tuple<Tp...> &, BaseNode& rhs) // Unused arguments are given no names.
-{ }
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-for_each_node(const std::tuple<Tp...>& t, BaseNode& rhs)
-{
-	std::get<I>(t) >> rhs;
-	for_each_node<I + 1, Tp...>(t, rhs);
-}
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-for_each_node_inv(BaseNode& rhs, const std::tuple<Tp...> &lhs ) // Unused arguments are given no names.
-{ }
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-for_each_node_inv(BaseNode& rhs, const std::tuple<Tp...>& lhs)
-{
-	rhs >> std::get<I>(lhs);
-	for_each_node_inv<I + 1, Tp...>(rhs, lhs);
-}
-
-template <std::size_t I = 0, typename ...Tp, typename ...Tn>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-for_each_node_dense(const std::tuple<Tp...>& lhs, const std::tuple<Tn...> rhs)
-{ }
-
-template <std::size_t I = 0, typename ...Tp, typename ...Tn>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-for_each_node_dense(const std::tuple<Tp...>& lhs, const std::tuple<Tn...> rhs)
-{
-	for_each_node_inv(std::get<I>(lhs), rhs);
-	for_each_node_dense<I + 1ul>(lhs,rhs);
-}
-
-inline std::tuple<BaseNode&, BaseNode&> operator,(BaseNode& lhs, BaseNode& rhs){
-	return std::forward_as_tuple(lhs, rhs);
-}
-
-template<typename...T1>
-inline std::tuple<T1&..., BaseNode&> operator,(const std::tuple<T1&...>& lhs, BaseNode& rhs){
-	return std::tuple_cat(lhs, std::forward_as_tuple(rhs));
-}
-
-template<typename... T1>
-inline BaseNode& operator>>(const std::tuple<T1&...>& lhs, BaseNode& rhs){
-	for_each_node(lhs, rhs);
+inline BaseNode& operator>>(const vec_BaseNode_ref& lhs, BaseNode& rhs){
+	std::for_each(lhs.begin(), lhs.end(), [&](BaseNode& a){
+		a >> rhs;
+	});
 	return rhs;
 }
 
-template<typename... T1>
-inline std::tuple<T1&...> operator>>(BaseNode& lhs, const std::tuple<T1&...>& rhs){
-	for_each_node_inv(lhs, rhs);
+inline vec_BaseNode_ref operator>>(BaseNode& lhs, const vec_BaseNode_ref& rhs){
+	std::for_each(rhs.begin(), rhs.end(), [&](BaseNode& a){
+		lhs >> a;
+	});
 	return rhs;
 }
 
-template <typename ...T1, typename ...T2>
-inline const std::tuple<T2...>& operator>>(const std::tuple<T1...>& lhs, const std::tuple<T2...>& rhs){
-	for_each_node_dense(lhs, rhs);
+inline vec_BaseNode_ref operator>>(vec_BaseNode_ref lhs, vec_BaseNode_ref rhs){
+	for (size_t i = 0; i < lhs.size() ; ++i){
+		lhs[i] >> rhs;
+	}
 	return rhs;
 }
+
+inline vec_BaseNode_ref operator,(BaseNode& lhs, BaseNode& rhs){
+	std::vector<BaseNode_ref> arr {lhs, rhs};
+	return arr;
+}
+
+inline vec_BaseNode_ref operator,(const vec_BaseNode_ref& lhs, BaseNode& rhs){
+	vec_BaseNode_ref vec(lhs);
+	
+	vec.emplace_back(rhs);
+	return vec;
+}
+
+// template<std::size_t I = 0, typename... Tp>
+// inline typename std::enable_if<I == sizeof...(Tp), void>::type
+// for_each_node(const std::tuple<Tp...> &, BaseNode& rhs) // Unused arguments are given no names.
+// { }
+
+// template<std::size_t I = 0, typename... Tp>
+// inline typename std::enable_if<I < sizeof...(Tp), void>::type
+// for_each_node(const std::tuple<Tp...>& t, BaseNode& rhs)
+// {
+// 	std::get<I>(t) >> rhs;
+// 	for_each_node<I + 1, Tp...>(t, rhs);
+// }
+
+// template<std::size_t I = 0, typename... Tp>
+// inline typename std::enable_if<I == sizeof...(Tp), void>::type
+// for_each_node_inv(BaseNode& rhs, const std::tuple<Tp...> &lhs ) // Unused arguments are given no names.
+// { }
+
+// template<std::size_t I = 0, typename... Tp>
+// inline typename std::enable_if<I < sizeof...(Tp), void>::type
+// for_each_node_inv(BaseNode& rhs, const std::tuple<Tp...>& lhs)
+// {
+// 	rhs >> std::get<I>(lhs);
+// 	for_each_node_inv<I + 1, Tp...>(rhs, lhs);
+// }
+
+// template <std::size_t I = 0, typename ...Tp, typename ...Tn>
+// inline typename std::enable_if<I == sizeof...(Tp), void>::type
+// for_each_node_dense(const std::tuple<Tp...>& lhs, const std::tuple<Tn...> rhs)
+// { }
+
+// template <std::size_t I = 0, typename ...Tp, typename ...Tn>
+// inline typename std::enable_if<I < sizeof...(Tp), void>::type
+// for_each_node_dense(const std::tuple<Tp...>& lhs, const std::tuple<Tn...> rhs)
+// {
+// 	for_each_node_inv(std::get<I>(lhs), rhs);
+// 	for_each_node_dense<I + 1ul>(lhs,rhs);
+// }
 
 } // namespace ends
